353a8d8fd74a3ef76b2bf645fa1b5f834cbb533a
Phil Hurwitz <pennstatephil@gmail.com>
2025-06-12 10:36:24 -0600

    Clear the active figure, if selected

--- src.js
+++ src.js
@@ -4,4 +4,5 @@ const wallGrid = grid.map(item => {
 	return (item === 2) ? item : 0; 
 })
 localStorage.setItem('grid', JSON.stringify(wallGrid))
+localStorage.setItem('active_figure_index', -1)
 window.location.href = 'https://gloom.aluminumangel.org/';
